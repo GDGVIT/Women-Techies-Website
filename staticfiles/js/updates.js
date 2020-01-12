662441be193a4c6bd7a77b34875ecfc4cf799986
Prakhar Soni <prakhar0912@gmail.com>
2020-01-12 23:43:39 +0530

    Invalid fields

--- staticfiles/js/updates.js
+++ staticfiles/js/updates.js
@@ -40,7 +40,7 @@ const isValid = (data) => {
 
 const handleUpdates = (e) => {
   console.log('[Event] updatesSubmitBtn clicked');
-  
+
   e.preventDefault();
   const data = {
     name: updatesInputName.value,
@@ -50,10 +50,77 @@ const handleUpdates = (e) => {
     eventName: 'WomenTechies20',
   };
   if (!isValid(data)) {
-    alert('Invalid fields');
+    let mes = "Invalid Fields";
+    const message = document.querySelector(".message");
+    var t1 = gsap.timeline();
+    t1.fromTo(".container", {
+      opacity: 1
+    }, {
+      opacity: 0.1,
+      duration: 0.5
+    });
+    t1.fromTo(".popup", {
+      display: "none",
+      opacity: 0
+    }, {
+      display: "block",
+      opacity: 1,
+      duration: 0.1
+    });
+    if (window.innerWidth > 992) {
+      t1.fromTo(".popup", {
+        width: 0
+      }, {
+        width: "20vw",
+        duration: 1,
+        ease: "power4.out"
+      });
+    }
+    else {
+      t1.fromTo(".popup", {
+        width: 0
+      }, {
+        width: "50vw",
+        duration: 1,
+        ease: "power4.out"
+      });
+    }
+
+    updatesSubmitBtn.innerHTML = "Submit"
+    if (window.innerWidth < 992) {
+      console.log(document.querySelector(".popup").style.height)
+      message.style.top = (40 * (window.innerHeight / 100)) - ((50 * (window.innerWidth / 100)) / 4.2) + "px";
+    }
+    else {
+      message.style.top = (50 * (window.innerHeight / 100)) - ((20 * (window.innerWidth / 100)) / 4.2) + "px";
+    }
+    message.innerHTML = mes;
+    message.style.color = "white";
+    t1.fromTo(".message", {
+      display: "none",
+      opacity: 0,
+    }, {
+      display: "block",
+      opacity: 1,
+      duration: 0.1
+    }, "-=0.5")
+    if (window.innerWidth > 992) {
+      setTimeout(() => {
+        message.style.display = "none";
+        document.querySelector(".popup").style.display = "none";
+        document.querySelector(".container").style.opacity = "1";
+      }, 2700);
+    }
+    else {
+      window.addEventListener("click", () => {
+        message.style.display = "none";
+        document.querySelector(".popup").style.display = "none";
+        document.querySelector(".container").style.opacity = "1";
+      })
+    }
     return;
   }
-  else{
+  else {
     updatesSubmitBtn.innerHTML = '<img src="./assets/loading.gif" alt="" height = "30" width = "25">'
   }
   const settings = {
@@ -71,88 +138,88 @@ const handleUpdates = (e) => {
     .then((data) => {
       let mes = "";
       const message = document.querySelector(".message");
-      
-      if(data.message == "Error: Document already exists"){
+
+      if (data.message == "Error: Document already exists") {
         mes = "Already Registered!";
       }
-      else{
+      else {
         console.log('Successfully submitted form');
         mes = "Seccessfully Submitted form!";
-        if(window.innerWidth > 992){
+        if (window.innerWidth > 992) {
           message.style.fontSize = "0.9vw"
         }
-        else{
+        else {
           message.style.fontSize = "2.2vw"
         }
       }
       var t1 = gsap.timeline();
       t1.fromTo(".container", {
         opacity: 1
-      },{
+      }, {
         opacity: 0.1,
         duration: 0.5
       });
       t1.fromTo(".popup", {
         display: "none",
         opacity: 0
-      },{
+      }, {
         display: "block",
         opacity: 1,
         duration: 0.1
       });
-      if(window.innerWidth > 992){
-        t1.fromTo(".popup",{
+      if (window.innerWidth > 992) {
+        t1.fromTo(".popup", {
           width: 0
-        },{
+        }, {
           width: "20vw",
           duration: 1,
           ease: "power4.out"
         });
       }
-      else{
-        t1.fromTo(".popup",{
+      else {
+        t1.fromTo(".popup", {
           width: 0
-        },{
+        }, {
           width: "50vw",
           duration: 1,
           ease: "power4.out"
         });
       }
-      
+
       updatesSubmitBtn.innerHTML = "Submit"
-      if(window.innerWidth < 992){
+      if (window.innerWidth < 992) {
         console.log(document.querySelector(".popup").style.height)
-        message.style.top = (40*(window.innerHeight/100)) - ((50*(window.innerWidth/100))/4.2) + "px";
+        message.style.top = (40 * (window.innerHeight / 100)) - ((50 * (window.innerWidth / 100)) / 4.2) + "px";
       }
-      else{
-        message.style.top = (50*(window.innerHeight/100)) - ((20*(window.innerWidth/100))/4.2) + "px";
+      else {
+        message.style.top = (50 * (window.innerHeight / 100)) - ((20 * (window.innerWidth / 100)) / 4.2) + "px";
       }
       message.innerHTML = mes;
       message.style.color = "white";
       t1.fromTo(".message", {
         display: "none",
         opacity: 0,
-      },{
+      }, {
         display: "block",
         opacity: 1,
         duration: 0.1
-      },"-=0.5")
-      if(window.innerWidth > 992){
+      }, "-=0.5")
+      if (window.innerWidth > 992) {
         setTimeout(() => {
           message.style.display = "none";
           document.querySelector(".popup").style.display = "none";
           document.querySelector(".container").style.opacity = "1";
         }, 5000);
       }
-      else{
-        window.addEventListener("click", ()=>{
+      else {
+        window.addEventListener("click", () => {
           message.style.display = "none";
           document.querySelector(".popup").style.display = "none";
           document.querySelector(".container").style.opacity = "1";
         })
       }
       console.log(data);
-      
+
     }).catch((err) => {
       alert('Error on submission');
       updatesSubmitBtn.innerHTML = "Submit";
