f4c2dde28f25146bfb086b2a029ea18cd197fa10
Prakhar Soni <prakhar0912@gmail.com>
2020-01-28 19:19:58 +0530

    nav-bar animation

--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,9 @@
 const button = document.querySelector(".ham");
 const navItems = document.querySelector(".nav-items");
+const lines = document.querySelectorAll(".ham div");
+console.log(lines[0]);
 button.addEventListener("click", ()=>{
     navItems.classList.toggle("open");
+    lines[0].classList.toggle("merge");
+    lines[1].classList.toggle("merge");
 });
