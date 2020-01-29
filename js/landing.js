d6fb9df98e72e428cfbffd646b4791e9b6eb50ce
Prakhar Soni <prakhar0912@gmail.com>
2020-01-29 22:06:34 +0530

    fourth responsive

--- js/landing.js
+++ js/landing.js
@@ -1,9 +1,11 @@
 const button = document.querySelector(".ham");
 const navItems = document.querySelector(".nav-items");
 const lines = document.querySelectorAll(".ham div");
-console.log(lines[0]);
 button.addEventListener("click", ()=>{
     navItems.classList.toggle("open");
     lines[0].classList.toggle("merge");
     lines[1].classList.toggle("merge");
+    console.log(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0)
 });
+
+
