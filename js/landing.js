b6e885d99826efba03a782900f7250d42960b4c0
Prakhar Soni <prakhar0912@gmail.com>
2020-01-30 00:35:35 +0530

    jumping

--- js/landing.js
+++ js/landing.js
@@ -13,7 +13,13 @@ window.addEventListener("resize", () => {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
     }
-}); 
+});
+navItems.addEventListener("click", ()=>{
+    setTimeout(()=>{
+        navItems.classList.toggle("open");
+    }, 900);
+})
+
 
 let scrollpos = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
 const vh = window.innerHeight;
