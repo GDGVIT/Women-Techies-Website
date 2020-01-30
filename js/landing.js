6021a1b21f5c6ce29247a50f6abf2aa522b3daff
Prakhar Soni <prakhar0912@gmail.com>
2020-01-30 20:28:19 +0530

    animation

--- js/landing.js
+++ js/landing.js
@@ -2,14 +2,30 @@ gsap.registerPlugin(ScrollToPlugin);
 const button = document.querySelector(".ham");
 const navItems = document.querySelector(".nav-items");
 const lines = document.querySelectorAll(".ham div");
-const navlinks = document.querySelectorAll(".nav-items div");
+const navlinks = document.querySelectorAll(".nav-items a");
 const body = document.querySelector("body");
 const ids = ["#one","#two","#three","#four","#five","#six"];
+const t1 = gsap.timeline();
+let i = 0;
 function openclose(){
     navItems.classList.toggle("open");
     lines[0].classList.toggle("merge");
     lines[1].classList.toggle("merge");
     body.classList.toggle("overflow");
+    if(i == 9){
+        i = 0;
+    }
+    else{
+        for(i = 0; i < 9; i++){
+            t1.fromTo(navlinks[i],{
+                opacity: 0
+            },{
+                opacity: 1,
+                duration: 0.10
+            })
+        }
+    }
+    console.log(i)
 }
 button.addEventListener("click", ()=>{
     openclose();
