85f144608a908968caad4e9947d5cd7cd05d7898
Prakhar Soni <prakhar0912@gmail.com>
2020-01-30 23:27:25 +0530

    elavator

--- js/landing.js
+++ js/landing.js
@@ -1,10 +1,12 @@
 gsap.registerPlugin(ScrollToPlugin);
 const button = document.querySelector(".ham");
 const navItems = document.querySelector(".nav-items");
+const navInner = document.querySelectorAll(".nav-inner a")
 const lines = document.querySelectorAll(".ham div");
 const navlinks = document.querySelectorAll(".nav-items a");
 const body = document.querySelector("body");
 const ids = ["#one","#two","#three","#four","#five","#six"];
+const colors = ["orange","rgb(164,27,228)","rgb(252,37,126)","rgb(164,27,228)","rgb(252,37,126)","rgb(164,27,228)"]
 const t1 = gsap.timeline();
 let i = 0;
 function openclose(){
@@ -30,21 +32,52 @@ function openclose(){
 button.addEventListener("click", ()=>{
     openclose();
 });
-let scrollpos = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
-const vh = window.innerHeight;
-let width = window.innerWidth;
-navlinks[0].addEventListener("click", ()=>{openclose()});
-navlinks[1].addEventListener("click", ()=>{openclose()});
-navlinks[4].addEventListener("click", ()=>{openclose()});
-navlinks[3].addEventListener("click", ()=>{openclose()});
-navlinks[2].addEventListener("click", ()=>{openclose()});
-navlinks[5].addEventListener("click", ()=>{openclose()});
 
-/* gsap.to(window, {duration: 1, scrollTo:{y:ids[5], offsetY:70}}); */
-/* function move(pos){
-    gsap.to(window, {duration: 0.1, ease: "power0", scrollTo: ids[pos]});
+let vh = window.innerHeight;
+let width = window.innerWidth;
+navlinks[0].addEventListener("click", ()=>{setTimeout(()=>{openclose()},1400)});
+navlinks[1].addEventListener("click", ()=>{setTimeout(()=>{openclose()},1400)});
+navlinks[4].addEventListener("click", ()=>{setTimeout(()=>{openclose()},1400)});
+navlinks[3].addEventListener("click", ()=>{setTimeout(()=>{openclose()},1400)});
+navlinks[2].addEventListener("click", ()=>{setTimeout(()=>{openclose()},1400)});
+navlinks[5].addEventListener("click", ()=>{setTimeout(()=>{openclose()},1400)});
+k = 0;
+if(width > 850){
+    window.addEventListener("scroll", (e)=>{
+        let scrollpos = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
+        scrollpos += 0.5*vh;
+        if(scrollpos > (k+1)*vh){
+            navInner[k].style.color = "black";
+            k++;
+            navInner[k].style.color = colors[k];
+        }
+        if(scrollpos < (k)*vh){
+            navInner[k].style.color = "black";
+            k--;
+            navInner[k].style.color = colors[k];
     
-} */
+        }
+    })
+}
+else{
+    let vh = window.innerHeight;
+    window.addEventListener("scroll", (e)=>{
+        let scrollpos = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
+        scrollpos += 0.4*vh;
+        if(scrollpos > (k+1)*vh){
+            navlinks[k].style.color = "black";
+            k++;
+            navlinks[k].style.color = colors[k];
+        }
+        if(scrollpos < (k)*vh){
+            navlinks[k].style.color = "black";
+            k--;
+            navlinks[k].style.color = colors[k];
+    
+        }
+    })
+}
+
 
 
 
