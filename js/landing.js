6fed4a63c3a8e9e9776476843883546714d47bb6
Prakhar Soni <prakhar0912@gmail.com>
2020-01-30 13:04:33 +0530

    hacking nav fix

--- js/landing.js
+++ js/landing.js
@@ -1,11 +1,13 @@
 const button = document.querySelector(".ham");
 const navItems = document.querySelector(".nav-items");
 const lines = document.querySelectorAll(".ham div");
+const body = document.querySelector("body");
 let i = 0;
 button.addEventListener("click", ()=>{
     navItems.classList.toggle("open");
     lines[0].classList.toggle("merge");
     lines[1].classList.toggle("merge");
+    body.style.overflow = "hidden";
 });
 navItems.addEventListener("click", ()=>{
     setTimeout(()=>{
@@ -13,8 +15,6 @@ navItems.addEventListener("click", ()=>{
     }, 900);
 })
 const nice = document.querySelector(".nice");
-
-
 
 let scrollpos = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
 const vh = window.innerHeight;
