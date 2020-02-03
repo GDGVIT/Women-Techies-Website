f22635519795ace05378e83626974210710e9924
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-02-04 04:39:56 +0530

    baseurl changed

--- staticfiles/js/config.js
+++ staticfiles/js/config.js
@@ -1,5 +1,4 @@
 const config = {
-  baseURLs: 'https://dsc-eventsreg.herokuapp.com/api/v1',
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://dsc-eventsreg.herokuapp.com/api/v1',
   URLBuilder: (packageName) => `${config.baseURL}/${packageName}/`,
 };
