c1cf9068883f934ba7d254d47b6135e414e117dc
Prakhar Soni <prakhar0912@gmail.com>
2020-02-05 20:36:42 +0530

    faq mobile and desktop and font changes

--- js/landing.js
+++ js/landing.js
@@ -10,9 +10,9 @@ const navInner = document.querySelectorAll(".nav-inner a")
 const lines = document.querySelectorAll(".ham div");
 const navlinks = document.querySelectorAll(".nav-items a");
 const body = document.querySelector("body");
-const colors = ["rgb(255, 167, 87)", "rgb(164,27,228)", "rgb(252,37,126)", "rgb(164,27,228)", "rgb(252,37,126)", "rgb(164,27,228)","rgb(255, 167, 87)","rgb(252,37,126)"]
+const colors = ["rgb(255, 167, 87)", "rgb(164,27,228)", "rgb(252,37,126)", "rgb(164,27,228)", "rgb(252,37,126)", "rgb(164,27,228)", "rgb(255, 167, 87)", "rgb(252,37,126)", "rgb(164,27,228)"]
 const t1 = gsap.timeline();
-let heights = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let heights = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 heights[0] = 0;
 heights[1] = document.querySelector("#one").clientHeight;
 heights[2] = document.querySelector("#two").clientHeight;
@@ -22,16 +22,25 @@ heights[5] = document.querySelector("#five").clientHeight;
 heights[6] = document.querySelector("#six").clientHeight;
 heights[7] = document.querySelector("#seven").clientHeight;
 heights[8] = document.querySelector("#eight").clientHeight;
-let height_color = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-let height_openclose = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-for (j = 0; j <= 8; j++) {
+heights[9] = document.querySelector("#nine").clientHeight;
+let height_color = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let height_openclose = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let height_color_desk = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+for (j = 0; j <= 9; j++) {
     for (o = 0; o <= j; o++) {
-        height_color[j] += heights[o];
+        if(o == 9){
+            height_color_desk[o] += 3*heights[j];
+            height_color[o] += 4*heights[j];
+        }
+        else{ 
+            height_color_desk[j] += heights[o];
+            height_color[j] += heights[o];
+        }
         if (o == j) {
-            height_openclose[j] += 0.8 * heights[o]
+            height_openclose[j] += 0.8 * heights[o];
         }
         else {
-            height_openclose[j] += heights[o]
+            height_openclose[j] += heights[o];
         }
     }
 }
@@ -70,24 +79,31 @@ navlinks[2].addEventListener("click", () => { a = 2; });
 navlinks[5].addEventListener("click", () => { a = 5; });
 navlinks[6].addEventListener("click", () => { a = 6; });
 navlinks[7].addEventListener("click", () => { a = 7; });
+navlinks[8].addEventListener("click", () => { a = 8; });
+
 k = 0;
 if (width > 850) {
-    window.addEventListener("resize", ()=>{
+    window.addEventListener("resize", () => {
         vh = window.innerHeight;
     })
     window.addEventListener("scroll", (e) => {
         let scrollpos = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
         scrollpos += 0.5 * vh;
-        if (scrollpos > (k + 1) * vh) {
+        if (scrollpos > height_color_desk[k+1]) {
             navInner[k].style.color = "black";
             k++;
+            console.log(k)
             navInner[k].style.color = colors[k];
+
         }
-        if (scrollpos < (k) * vh) {
+        if (scrollpos < height_color_desk[k]) {
             navInner[k].style.color = "black";
             k--;
+            console.log(k)
             navInner[k].style.color = colors[k];
+
         }
+
     })
 }
 else {
@@ -114,6 +130,20 @@ else {
             }
         }
     })
+}
+var acc = document.getElementsByClassName("accordion");
+var q;
+
+for (q = 0; q < acc.length; q++) {
+    acc[q].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
 
 
